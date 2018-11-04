893f5e739a4eb4324ad2e999121ea011a766d219
Christopher <clemonde28@gmail.com>
2018-11-03 22:39:38 -0400

    added basic leaderboard

---
+++ Models/Entry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhisperLeaderboard.Models
+{
+    public class Entry
+    {
+        public string Name1 { get; }
+        public string Name2 { get; }
+        public int Score { get; }
+
+        public Entry(string name1, string name2, int score)
+        {
+            Name1 = name1;
+            Name2 = name2;
+            Score = score;
+        }
+    }
+}
---
+++ Models/Leaderboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhisperLeaderboard.Models
+{
+    public class Leaderboard
+    {
+        public List<Entry> Entries { get; }
+
+        public Leaderboard()
+        {
+            Entries = new List<Entry>();
+            Entries.Add(new Entry("Chantale", "Bolduc", 203));
+        }
+    }
+}
