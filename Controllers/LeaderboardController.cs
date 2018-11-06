d1e44748e9fc7df0bcbd754d29ec8b47deaa2438
Christopher <clemonde28@gmail.com>
2018-11-06 18:45:36 -0500

    Added SignalR in application

--- Controllers/LeaderboardController.cs
+++ Controllers/LeaderboardController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.SignalR;
 using WhisperLeaderboard.Models;
 using WhisperLeaderboard.Models.Dto;
 
@@ -9,6 +9,12 @@ namespace WhisperLeaderboard.Controllers
     public class LeaderboardController : Controller
     {
         private static Leaderboard _leaderboard = new Leaderboard();
+        private static IHubContext<LeaderboardHub> _hubContext;
+
+        public LeaderboardController(IHubContext<LeaderboardHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
 
         [HttpGet]
         public IActionResult GetLeaderboard()
@@ -20,6 +26,7 @@ namespace WhisperLeaderboard.Controllers
         public IActionResult AddNewEntry([FromBody] Entry entry)
         {
             _leaderboard.InsertEntry(entry.Name1, entry.Name2, entry.Score);
+            _hubContext.Clients.All.SendAsync("Update");
             return Ok("Entry added");
         }
 
@@ -33,7 +40,7 @@ namespace WhisperLeaderboard.Controllers
         public IActionResult InitializeNewLeadeboard([FromBody] LeaderboardDto leaderboardDto)
         {
             _leaderboard = new Leaderboard(leaderboardDto.Entries, leaderboardDto.Size);
-            return Ok("Initialized Leaderboard");
+            return Ok("Initialized leaderboard");
         }
     }
 }
---
+++ wwwroot/js/leaderboard.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/leaderboardHub").build();
+
+connection.on("Update", function () {
+    $("#mytable").load("Leaderboard #mytable");
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
